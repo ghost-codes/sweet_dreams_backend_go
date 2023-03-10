cbb227e5e6b6936dac082ee54b00a38de3bff650
Hope Dorkenoo <compounddork@gmail.com>
2023-03-10 12:24:37 +0000

    add config file

---
+++ util/config.go
@@ -0,0 +1,44 @@
+package util
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+	SecretKey            string        `mapstructure:"SECRET_KEY"`
+	DBDriver             string        `mapstructure:"DB_DRIVER"`
+	ServerAddr           string        `mapstructure:"SEVER_ADDR"`
+	DBHOST               string        `mapstructure:"DB_HOST"`
+	DBPort               string        `mapstructure:"DB_PORT"`
+	DBUser               string        `mapstructure:"DB_USER"`
+	DBPassword           string        `mapstructure:"DB_PASSWORD"`
+	DBName               string        `mapstructure:"DB_NAME"`
+
+	// DBSource            string        `mapstructure:"DB_SOURCE"`
+}
+
+func (config *Config) DBSource() string {
+	return fmt.Sprintf("postgresql://%v:%v@%v:%v/%v?sslmode=disable", config.DBUser, config.DBPassword, config.DBHOST, config.DBPort, config.DBName)
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(config)
+
+	return config, err
+
+}
