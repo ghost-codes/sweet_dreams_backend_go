630588b53ccd6532adaeb33eeb195348af028262
Hope Dorkenoo <compounddork@gmail.com>
2023-03-22 20:26:53 +0000

    minor start.sh change

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "run db migration"
 echo "=====> $DB_SOURCE"
-/app/migrate -path /app/migration -database "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable" -verbose up
+/app/migrate -path /app/migrations -database "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable" -verbose up
 
 echo "start up"
 exec "$@"
