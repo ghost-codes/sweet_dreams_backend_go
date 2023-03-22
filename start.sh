46ceb17603c44e3cb735ee4fc52b15ef202d7e60
Hope Dorkenoo <compounddork@gmail.com>
2023-03-22 21:49:52 +0000

    a

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 set -e
+FILE=/app/.env
+if [ ! -f "$FILE" ]; then
+    touch .env
+fi
 
 echo "run db migration"
 echo "=====> $DB_SOURCE"
 /app/migrate -path /app/migrations -database "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable" -verbose up
+
 
 echo "start up"
 exec "$@"
